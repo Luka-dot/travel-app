300310f7de0bcc702c6ff62474209e24eed9a216
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-24 20:33:52 -0400

    router set up

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewPlace extends Component {
+    render() {
+        return (
+            <div>
+                <h1> New Place Rocks!!!!</h1>
+            </div>
+        )
+    }
+}
+
+export default NewPlace
