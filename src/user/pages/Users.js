8ec0bee6570330183f129583a141cf94fad7f6d0
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-25 10:29:49 -0400

    Users set up and user list rendering... no styling yet

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-    return <h1> Users ROCKS!!!</h1>;
+    const USERS = [{
+        id: 'u1', 
+        name: 'Lukas', 
+        image: 'https://www.planetware.com/wpimages/2019/06/czech-republic-prague-itineraries-for-travelers-one-day-itinerary-old-town-square.jpg',
+        places: 5
+    },
+    {
+        id: 'u2', 
+        name: 'Max', 
+        image: 'https://i.redd.it/q2f9xfhtvfb31.jpg',
+        places: 3
+    }];
+    return <UsersList items={USERS} />;
 }
 
 export default Users;
