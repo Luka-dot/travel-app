8b5e6408fc4f8a7fbb905f4e446ae8fcce80f6e9
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-24 20:38:32 -0400

    router set up and folder structure

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+import './UsersList.css';
+
+const UserItem = props => {};
+
+export default UserItem;
---
+++ src/user/components/UsersList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './UserItem.css';
+
+export class UsersList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UsersList
