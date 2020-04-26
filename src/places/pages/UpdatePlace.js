0fd1b2973dc041e1d6de91ca9781a38c0b52095f
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-25 22:26:52 -0400

    code clean up for readibility

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -10,55 +10,55 @@ import Card from '../../shared/components/UIElements/Card';
 
 const DUMMY_PLACES = [
     {
-      id: 'p1',
-      title: 'Empire State Building',
-      description: 'One of the most famous sky scrapers in the world!',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-      address: '20 W 34th St, New York, NY 10001',
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584
-      },
-      creator: 'u1'
-    },
-    
-      {
-          id: 'p3',
-          title: 'Prague Old Town Square',
-          description: 'Old Town Square in Prague Czech rep.',
-          imageUrl: 'https://www.planetware.com/wpimages/2019/06/czech-republic-prague-itineraries-for-travelers-one-day-itinerary-old-town-square.jpg',
-          address: 'Staroměstské nám., 110 00 Josefov, Czechia',
-          location: {
-              lat: 50.0894,
-              lng: 14.4194
-            },
-          creator: 'u1'
-      },
-      {
-          id: 'p4',
-          title: 'Cesky Krumplov',
-          description: 'Old Town of Cesky Krumlov. Town in Czech rep.',
-          imageUrl: 'https://i.redd.it/q2f9xfhtvfb31.jpg',
-          address: '381 01 Český Krumlov, Czechia',
-          location: {
-              lat: 48.81091,
-              lng: 14.31521
-            },
-          creator: 'u2'
-      },
-      {
-          id: 'p2',
-          title: 'Empire State Building',
-          description: 'One of the most famous sky scrapers in the world!',
-          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-          address: '20 W 34th St, New York, NY 10001',
-          location: {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
             lat: 40.7484405,
             lng: -73.9878584
-          },
-          creator: 'u2'
         },
-  ];
+        creator: 'u1'
+    },
+
+    {
+        id: 'p3',
+        title: 'Prague Old Town Square',
+        description: 'Old Town Square in Prague Czech rep.',
+        imageUrl: 'https://www.planetware.com/wpimages/2019/06/czech-republic-prague-itineraries-for-travelers-one-day-itinerary-old-town-square.jpg',
+        address: 'Staroměstské nám., 110 00 Josefov, Czechia',
+        location: {
+            lat: 50.0894,
+            lng: 14.4194
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p4',
+        title: 'Cesky Krumplov',
+        description: 'Old Town of Cesky Krumlov. Town in Czech rep.',
+        imageUrl: 'https://i.redd.it/q2f9xfhtvfb31.jpg',
+        address: '381 01 Český Krumlov, Czechia',
+        location: {
+            lat: 48.81091,
+            lng: 14.31521
+        },
+        creator: 'u2'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world!',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+        address: '20 W 34th St, New York, NY 10001',
+        location: {
+            lat: 40.7484405,
+            lng: -73.9878584
+        },
+        creator: 'u2'
+    },
+];
 
 const UpdatePlace = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -93,7 +93,7 @@ const UpdatePlace = () => {
         }
         setIsLoading(false);
     }, [setFormData, identifiedPlace]);
-    
+
 
     const placeUpdateSubmitHandler = event => {
         event.preventDefault();
@@ -104,7 +104,7 @@ const UpdatePlace = () => {
         return (
             <div className="center">
                 <Card>
-                <h2>Couldnt find place</h2>
+                    <h2>Couldnt find place</h2>
                 </Card>
             </div>
         );
@@ -112,40 +112,40 @@ const UpdatePlace = () => {
     if (isLoading) {
         return (
             <div className="center">
-        <h1> ......LOADING......</h1>
-        </div>
+                <h1> ......LOADING......</h1>
+            </div>
         )
     }
 
     return (
-         <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
-        <Input 
-            id="title" 
-            element="input"
-            type="text" 
-            label="Title"
-            validators={[VALIDATOR_REQUIRE()]} 
-            errorText="Please enter valid title"
-            onInput={inputHandler}
-            initialValue={formState.inputs.title.value}
-            initialValid={formState.inputs.title.isValid}
+        <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+            <Input
+                id="title"
+                element="input"
+                type="text"
+                label="Title"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter valid title"
+                onInput={inputHandler}
+                initialValue={formState.inputs.title.value}
+                initialValid={formState.inputs.title.isValid}
             />
-            <Input 
-            id="description" 
-            element="textarea" 
-            label="Description"
-            validators={[VALIDATOR_MINLENGTH(5)]} 
-            errorText="Please enter valid description (min 5 characters"
-            onInput={inputHandler}
-            initialValue={formState.inputs.description.value}
-            initialValid={formState.inputs.description.isValid}
+            <Input
+                id="description"
+                element="textarea"
+                label="Description"
+                validators={[VALIDATOR_MINLENGTH(5)]}
+                errorText="Please enter valid description (min 5 characters"
+                onInput={inputHandler}
+                initialValue={formState.inputs.description.value}
+                initialValid={formState.inputs.description.isValid}
             />
-            <Button 
+            <Button
                 type="submit"
                 disabled={!formState.isValid}
-                >UPDATE PLACE</Button>
-    </form>
-        
+            >UPDATE PLACE</Button>
+        </form>
+
     )
 }
 
