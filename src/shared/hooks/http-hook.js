1f722c8f48bc7e32aee524c8b068db619b0cd10b
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-27 15:01:25 -0400

    users log in and creation account is working

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -9,6 +9,7 @@ export const useHttpClient = () => {
   const sendRequest = useCallback(
     async (url, method = 'GET', body = null, headers = {}) => {
       setIsLoading(true);
+      // httpAbortCtrl is functionality available in modern browsers
       const httpAbortCtrl = new AbortController();
       activeHttpRequests.current.push(httpAbortCtrl);
 
