8a891b580e2d6e56eaddc6f72a499b909db67ebb
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-27 13:08:36 -0400

    create user is set up and working with front-end

--- back-end/app.js
+++ back-end/app.js
@@ -10,7 +10,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes); // => /api/places...
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+})
+
+app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
@@ -22,15 +29,16 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 mongoose
-  .connect('mongodb+srv://Main-user:12345@cluster0-uqk4f.mongodb.net/test?retryWrites=true&w=majority')
+.connect('mongodb+srv://Main-user:12345@cluster0-uqk4f.mongodb.net/test?retryWrites=true&w=majority')
   .then(() => {
     app.listen(5000);
   })
   .catch(err => {
     console.log(err);
   });
+
