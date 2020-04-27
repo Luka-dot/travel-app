2f6ef6838afaa4a2f993c8cb91e295307194547e
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-27 17:27:32 -0400

    adding place is successfull, need redirect after add

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -34,23 +34,23 @@ const Auth = () => {
     false
   );
 
-   const switchModeHandler = () => {
-     if (!isLoginMode) {
-       setFormData({
-          ...formState.inputs,
-          name: undefined
-       }, formState.inputs.email.isValid && formState.inputs.password.isValid)
-     } else {
-       setFormData({
-         ...formState.inputs,
-         name: {
+  const switchModeHandler = () => {
+    if (!isLoginMode) {
+      setFormData({
+        ...formState.inputs,
+        name: undefined
+      }, formState.inputs.email.isValid && formState.inputs.password.isValid)
+    } else {
+      setFormData({
+        ...formState.inputs,
+        name: {
           value: '',
           isValid: false
         }
-       }, false)
-     }
+      }, false)
+    }
     setIsLoginMode(prevMode => !prevMode);
-   };
+  };
 
   const authSubmitHandler = async event => {
     event.preventDefault();
@@ -58,27 +58,27 @@ const Auth = () => {
     if (isLoginMode) {
       try {
         // *************** LOGIN ************************
-        await sendRequest(
-          'http://localhost:5000/api/users/login', 
+        const responseData = await sendRequest(
+          'http://localhost:5000/api/users/login',
           'POST',
           JSON.stringify({
             email: formState.inputs.email.value,
             password: formState.inputs.password.value
-            }),
+          }),
           {
             'Content-Type': 'application/json'
           },
         );
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {
         console.log(err);
       }
-      } else {
-        try {
+    } else {
+      try {
         // *************** SIGNUP ************************
-        await sendRequest(
-          'http://localhost:5000/api/users/signup', 
-          'POST', 
+        const responseData = await sendRequest(
+          'http://localhost:5000/api/users/signup',
+          'POST',
           JSON.stringify({
             name: formState.inputs.name.value,
             email: formState.inputs.email.value,
@@ -86,57 +86,57 @@ const Auth = () => {
           }),
           {
             'Content-Type': 'application/json'
-        }       
-      );  
-        auth.login();
-  
-      } catch (err) {}
-    } 
+          }
+        );
+        auth.login(responseData.user.id);
+
+      } catch (err) { }
+    }
   };
 
 
   return (
     <React.Fragment>
       <ErrorModal error={error} onClear={clearError} />
-    <Card className="authentication">
-      {isLoading && <LoadingSpinner asOverlay={true}/>}
-      <h2>Login Required</h2>
-      <hr />
-      <form onSubmit={authSubmitHandler}>
-        {!isLoginMode && <Input 
-                              element="input"
-                              id="name"
-                              type="text"
-                              label="Your Name"
-                              validators={[VALIDATOR_REQUIRE()]}
-                              errorText="Please enter a name"
-                              onInput={inputHandler} 
-                          />
-        }
-        <Input
-          element="input"
-          id="email"
-          type="email"
-          label="E-Mail"
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Please enter a valid email address."
-          onInput={inputHandler}
-        />
-        <Input
-          element="input"
-          id="password"
-          type="password"
-          label="Password"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please enter a valid password, at least 5 characters."
-          onInput={inputHandler}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLoginMode ? 'LOGIN ' : 'SIGNUP'}
-        </Button>
-      </form>
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay={true} />}
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLoginMode && <Input
+            element="input"
+            id="name"
+            type="text"
+            label="Your Name"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a name"
+            onInput={inputHandler}
+          />
+          }
+          <Input
+            element="input"
+            id="email"
+            type="email"
+            label="E-Mail"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please enter a valid email address."
+            onInput={inputHandler}
+          />
+          <Input
+            element="input"
+            id="password"
+            type="password"
+            label="Password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please enter a valid password, at least 5 characters."
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginMode ? 'LOGIN ' : 'SIGNUP'}
+          </Button>
+        </form>
         <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}</Button>
-    </Card>
+      </Card>
     </React.Fragment>
   );
 };
