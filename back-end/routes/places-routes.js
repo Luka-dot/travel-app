eaf4e2228f6806c5e954cbe6d6f20c3e508adc5b
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-29 11:54:12 -0400

    routs are protected on backend with JWT

--- back-end/routes/places-routes.js
+++ back-end/routes/places-routes.js
@@ -3,12 +3,16 @@ const { check } = require('express-validator');
 
 const placesControllers = require('../controllers/places-controllers');
 const fileUpload = require('../middleware/file-upload');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
 router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
+
+// middleware to make sure routs below needs JWT
+router.use(checkAuth);
 
 router.post(
   '/',
